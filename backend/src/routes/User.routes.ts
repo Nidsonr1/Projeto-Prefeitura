f10a618c0ac995b45816a4f138d89b020f1466dc
Nidsonr1 <nidsoncampos@hotmail.com>
2021-11-25 22:57:50 -0300

    Rota para perfil do usuário

--- backend/src/routes/User.routes.ts
+++ backend/src/routes/User.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import createUserController  from "../useCases/createUser/";
 import turnAdminController from "../useCases/turnAdmin";
 import loginUserController from '../useCases/loginUser';
+import profile from '../useCases/profile';
 import { checkAdmin } from "../middlewares/CheckAdmin";
 
 const userRoutes = Router();
@@ -15,8 +16,12 @@ userRoutes.patch("/:id/admin", checkAdmin, (request, response) => {
   return turnAdminController().handle(request, response);
 });
 
-userRoutes.post('/login', (request, response) => {
+userRoutes.post("/login", (request, response) => {
   return loginUserController().handle(request, response);
 });
+
+userRoutes.get("/profile", (request, response) => {
+  return profile().handle(request, response);
+})
 
 export { userRoutes }
